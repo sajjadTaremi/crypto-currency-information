bf5122d1bfaefeb68d2c5c25836204ca59f479ca
Your Name <sajjadtaremi99@gmail.com>
2022-07-17 17:07:53 +0430

    icon add nav

--- src/Components/Navigation/MenuItems.js
+++ src/Components/Navigation/MenuItems.js
@@ -1,26 +1,32 @@
+import { AiOutlineHome } from "react-icons/ai";
+import { BiPhoneIncoming } from "react-icons/bi";
 const menuItems = [
   {
     title: "صفحه اصلی",
     url: "/",
     cName: "nav-links",
+    icon: <AiOutlineHome />,
   },
   {
     title: "سبد خرید",
     url: "cart",
     cName: "nav-links",
+    // icon: <AiOutlineHome />,
   },
   {
     title: "محصولات",
     url: "product",
     cName: "nav-links",
+    // icon: <AiOutlineHome />,
   },
   {
     title: "تماس با من",
     url: "about",
     cName: "nav-links",
+    icon: <BiPhoneIncoming />,
   },
   {
-    title: "ثبت نام",
+    title: "ثبت نام / ورود",
     url: "singup",
     cName: "nav-links-mobile",
   },
--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import menuItems from "../Navigation/MenuItems";
+import { FaAtlassian } from "react-icons/fa";
 import "./Navigation.css";
+
 const Navigation = () => {
   const [active, setActive] = useState(false);
 
@@ -12,6 +14,9 @@ const Navigation = () => {
   return (
     <header>
       <nav className="navbar">
+        <span className="navbar-logo">
+          <i>{<FaAtlassian />}</i>
+        </span>
         <h1 className="navbar-logo">اطلاعات آنلاین فارسی</h1>
         <div className="menu-icon" onClick={handleClick}>
           <i className={active ? "fas fa-times" : "fas fa-bars"}></i>
@@ -21,6 +26,9 @@ const Navigation = () => {
             return (
               <li key={index}>
                 <NavLink to={item.url} className={item.cName}>
+                  <span className="ccc">
+                    <i>{item.icon}</i>
+                  </span>
                   {item.title}
                 </NavLink>
               </li>
